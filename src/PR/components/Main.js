fc8b7cd10e0c6388f37a9bc47a68f4feaf648d61
alexgharibi <alex.morin.gh@gmail.com>
2021-10-06 14:28:05 -0400

    aaaa

--- src/PR/components/Main.js
+++ src/PR/components/Main.js
@@ -1,64 +1,61 @@
-import React, { useContext } from "react";
+import React, { useState } from "react";
+import logo1 from "../images/shirt-1.jpg";
 import classes from "./Main.module.css";
-import { shirts } from "./shirtData";
-import StarsRating from "./StarsRating";
-import { Cart } from "./Cart";
-import { CartContext } from "./CartContext";
+import { FaStar } from "react-icons/fa";
+
+const colors = {
+  orange: "#FFBA5A",
+  grey: "#a9a9a9",
+};
 
 const Main = () => {
-  const [cart, setCart] = useContext(CartContext);
+  const [starValue, setStarValue] = useState(0);
+  const [hoverValue, setHoverValue] = useState(undefined);
+  const stars = Array(5).fill(0);
 
-  const addToCart = () => {
-    const shirtsList = { title: shirts.title, amount: shirts.amount };
-
-    setCart((curr) => [...curr, shirtsList]);
+  const handleClick = (value) => {
+    setStarValue(value);
   };
-  const renderCard = (shirt) => {
-    return (
-      <div className={classes.block} key={shirt.id}>
-        <img src={shirt.image} alt="shirt" />
-        <div className={classes["block-p"]}>{shirt.title}</div>
-        <div className={classes["block-p"]}>{shirt.amount}</div>
-        <button onClick={addToCart} className={classes.btn}>
-          Add to Card
-        </button>
-        <StarsRating />
-        <Cart />
-      </div>
-    );
+
+  const handleHover = (value) => {
+    setHoverValue(value);
+  };
+
+  const handleHoverLeave = (value) => {
+    setHoverValue(undefined);
   };
 
   return (
     <React.Fragment>
-      <div className={classes.grid}>{shirts.map(renderCard)}</div>
+      <div className={classes.grid}>
+        <div className={classes.block}>
+          <img src={logo1} alt="shirt" />
+          <div className={classes["block-p"]}>Ultrafine Merino T-Shirt</div>
+          <div className={classes["block-p"]}>$80.00</div>
+          <button className={classes.btn}>Add to Card</button>
+          <div>
+            {stars.map((_, index) => {
+              return (
+                <FaStar
+                  key={index}
+                  size={24}
+                  style={{ marginRight: 10, cursor: "pointer" }}
+                  color={
+                    (hoverValue || starValue) > index
+                      ? colors.orange
+                      : colors.grey
+                  }
+                  onClick={() => handleClick(index + 1)}
+                  onMouseOver={() => handleHover(index + 1)}
+                  onMouseLeave={() => handleHoverLeave(index + 1)}
+                />
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </React.Fragment>
   );
 };
 
 export default Main;
-
-// const card = (
-//   <div className={classes.block}>
-//     <img src={logo1} alt="shirt" />
-//     <div className={classes["block-p"]}>Ultrafine Merino T-Shirt</div>
-//     <div className={classes["block-p"]}>$80.00</div>
-//     <button className={classes.btn}>Add to Card</button>
-//     <div>
-//       {stars.map((_, index) => {
-//         return (
-//           <FaStar
-//             key={index}
-//             size={24}
-//             style={{ marginRight: 10, cursor: "pointer" }}
-//             color={
-//               (hoverValue || starValue) > index ? colors.orange : colors.grey
-//             }
-//             onClick={() => handleClick(index + 1)}
-//             onMouseOver={() => handleHover(index + 1)}
-//             onMouseLeave={() => handleHoverLeave(index + 1)}
-//           />
-//         );
-//       })}
-//     </div>
-//   </div>
-// );
